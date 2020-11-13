1e9a3799fee08a86ffa1cf9a0e3213857eec24bb
RubenV-Dev <r.will16@hotmail.com>
2020-11-13 13:10:23 -0500

    added conditional rendering of card info

--- src/Components/drawingcard.js
+++ src/Components/drawingcard.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+const displayInfo = (isShown, props) => {
+    if (isShown) {
+        return (
+            <div className="card-info" >
+                <p>{props.description}</p>
+                <p>Image Owner</p>
+                <p>Likes</p>
+            </div>
+        )
+    }
+}
 
 const DrawingCard = (props) => {
+    const [isShown, setIsShown] = useState(false)
     return (
-        <div className="drawing-card" style={{backgroundImage: `url(${props.img})`, backgroundSize: 'cover' ,height: "200px", width: "200px" }}>
-             <p>Im a card</p>
+        <div className="drawing-card" 
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)} 
+        style={{backgroundImage: `url(${props.img})`, backgroundSize: 'cover' ,height: "200px", width: "200px" }}>
+                {isShown &&
+                <div className="card-info" >
+                    <p>{props.description}</p>
+                    <p>Image Owner</p>
+                    <p>Likes</p>
+                </div>}
         </div>
     )
 }
